46d32cc851e2d6e57d84467f0a4b6bc5e689c36a
Nadro-j <n4dro@protonmail.com>
2021-07-20 22:36:59 +0000

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py-subalert',
+    version='0.0.1',
+    packages=['subalert'],
+    url='https://github.com/Nadro-J/py-subalert',
+    license='',
+    author='N4DRO',
+    author_email='n4dro@protonmail.com',
+    description='Python Substrate Interface Library for automated alerts '
+)
