e51f13f5ed7812fa41e5f60093e05d77ed038acf
nadro-j <n4dro@protonmail.com>
2021-07-25 03:13:16 +0000

    cronjob comments added

--- scripts/validator-commission.py
+++ scripts/validator-commission.py
@@ -1,3 +1,4 @@
+# 0 */12 * * * cd /root/py-subalert && /usr/bin/python3 /root/py-subalert/commission.py >> /root/py-subalert/validator_commission.log 2>&1
 from subalert.validator import *
 
 validators = ValidatorWatch()
