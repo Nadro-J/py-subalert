6f97910a44329a2489e59f116d5582a77892128b
nadro-j <n4dro@protonmail.com>
2021-10-03 00:08:00 +0000

    new script added to take frequent snapshots of validator commission

---
+++ scripts/validator-analysis.py
@@ -0,0 +1,14 @@
+from subalert.validator import ValidatorWatch
+import subalert.base
+import datetime
+import json
+
+vw = ValidatorWatch()
+print("Getting validator commission.. please wait")
+data = vw.get_current_commission()
+timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%I%M%S%p")
+
+print("Writing data to directory")
+subalert.base.Utils.cache_data(f'validator-analysis/snapshot-{timestamp}.json', data)
+
+print("Complete!")
