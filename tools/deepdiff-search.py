65e4b87b0454113dca6dce9faf9753142cb0023d
nadro-j <n4dro@protonmail.com>
2021-10-04 00:38:56 +0000

    convert commission to readable format, identify validators/1kv

--- tools/deepdiff-search.py
+++ tools/deepdiff-search.py
@@ -6,6 +6,8 @@ import subalert.base
 from pathlib import Path
 
 paths = sorted(Path('../validator-analysis').iterdir(), key=os.path.getmtime)
+_1kv_candidates = subalert.base.Utils.get_1kv_candidates()
+subquery = subalert.base.SubQuery()
 
 for i in range(0, len(paths)):
     x = i
@@ -15,21 +17,39 @@ for i in range(0, len(paths)):
         y_path = subalert.base.Utils.open_cache(paths[y])
         x_path = subalert.base.Utils.open_cache(paths[x])
 
-        print(f"================\n"
-              f"Comparing: '{paths[y]}'\n"
-              f"Against: '{paths[x]}'\n"
-              f"----------------")
+        print(f"\nComparing: '{paths[y]}'\n"
+              f"Against: '{paths[x]}'")
 
         difference = deepdiff.DeepDiff(y_path, x_path, ignore_order=True).to_json()
         result = json.loads(difference)
+        validators_list = {}
 
         if len(result) == 0:
-            print(f"No changes made during this time period.\n"
-                  f"================\n\n")
             continue
 
         for key, value in result.items():
-            print(f"key: {key}\n"
-                  f"values_changed: {value}")
-        print(f"================\n\n")
-        time.sleep(3.14)
+            if key == 'values_changed':
+                for obj, attributes in result['values_changed'].items():
+                    if 'blocked' in obj:
+                        pass
+                    elif 'commission' in obj:
+                        address = obj.replace("root['", "").replace("']", "").replace("['commission", "")
+                        validators_list.update({address: attributes})
+
+                for validator_address, changed_attributes in validators_list.items():
+                    _1kv = False
+                    validators_list[validator_address]['old_value'] = float(f"{100 * float(changed_attributes['old_value']) / float(1000000000):,.2f}")
+                    validators_list[validator_address]['new_value'] = float(f"{100 * float(changed_attributes['new_value']) / float(1000000000):,.2f}")
+
+                    if validator_address in _1kv_candidates:
+                        _1kv = True
+                        validators_list[validator_address]['is1kv'] = _1kv
+
+                    identity = subquery.check_identity(validator_address)
+                    if identity:
+                        validators_list[validator_address]['identity'] = identity
+
+                    print("----------------")
+                    print(validator_address)
+                    print(changed_attributes)
+                print(f"================")
