d56c36f2aaf1d5cba563a7ff9b1046753a690c3e
nadro-j <n4dro@protonmail.com>
2021-10-17 20:24:25 +0000

    removing unnecessary files

--- main.py
+++
@@ -1,5 +0,0 @@
-from subalert.transact import *
-
-# subscribe to transactions > transact_threshold : specified in config.local.yaml
-txSub = TransactionSubscription()
-txSub.substrate.subscribe_block_headers(txSub.new_block, include_author=True)
--- phragmen_test.py
+++
@@ -1,5 +0,0 @@
-from subalert.phragmen_election import *
-
-a = PhragmenSubscription()
-print(a.get_candidates())
-#print(a.voting_info())
