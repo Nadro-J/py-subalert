bb5388d6bbd8ca1dd3f5c4eb4e28d3f765a85a43
Nadro-j <n4dro@protonmail.com>
2021-10-17 21:44:43 +0000

    removing redundant code

--- deploy.sh
+++ deploy.sh
@@ -28,17 +28,3 @@ echo "[+] Creating cronjobs if they haven't been created already"
 (crontab -l; echo "0 17 * * * cd /$user/py-subalert && /usr/bin/python3 /$user/py-subalert/era-graph.py >> /$user/py-subalert/logs/era-graph.log 2>&1") | sort -u | crontab -
 (crontab -l; echo "0 * * * * cd /$user/py-subalert && /usr/bin/python3 /$user/py-subalert/tip-alert.py >> /$user/py-subalert/logs/tip-alert.log 2>&1") | sort -u | crontab -
 (crontab -l; echo "0 * * * * cd /$user/py-subalert && /usr/bin/python3 /$user/py-subalert/latest_repo_release.py >> /$user/py-subalert/logs/latest_repo_release.log 2>&1") | sort -u | crontab -
-
-if [ ! -d /$user/py-subalert/logs ]; then
-	echo "[+] Creating /logs directory for cronjob logging"
-	mkdir -p /$user/py-subalert/logs;
-else
-  echo "[!] /logs directory already exists"
-fi
-
-if [ ! -d /$user/py-subalert/data-cache ]; then
-	echo "[+] Creating /data-cache directory to store data"
-	mkdir -p /$user/py-subalert/data-cache;
-else
-  echo "[!] /data-cache diretory already exists"
-fi
