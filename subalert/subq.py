06e34c9595405d9cd464b268ad98c5f1b83c81f1
nadro-j <n4dro@protonmail.com>
2022-07-08 00:56:50 +0000

    split classes from base file

---
+++ subalert/subq.py
@@ -0,0 +1,39 @@
+from subalert.subtweet import Tweet
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        d = self.items[0]
+        # list comprehension
+        return sum([len(d[x]) for x in d if isinstance(d[x], list)])
+
+    def clear(self):
+        return self.items.clear()
+
+    async def process_queue(self):
+        print("+++ process_queue called")
+        counter = 0
+
+        if len(self.items[0]['batch_all']) >= 1 and self.items[0]['batch_all'][0] is not None:
+            for tweet, media in self.items[0]['batch_all']:
+                counter += 1
+                print(f"\n##[ batch_all ({counter}) ]###\n{tweet}\n{media}\n------\n")
+                #Tweet("NonFungibleTxs").alert(message=tweet, filename=media, verbose=True)
+
+        if len(self.items[0]['transactions']) >= 1 and self.items[0]['transactions'][0] is not None:
+            for tx in self.items[0]['transactions']:
+                counter += 1
+                print(f"##[ transaction ({counter}) ]###\n{tx}")
+                #Tweet("KusamaTxs").alert(message=tx, verbose=True)
---
+++ subalert/subtweet.py
@@ -0,0 +1,45 @@
+from subalert.config import Configuration
+import tweepy
+import time
+
+config = Configuration()
+
+
+class Tweet:
+    def __init__(self, account):
+        self.account = account
+        if self.account not in config.yaml_file['twitter']['sub_twitter']:
+            pass
+
+        consumer_key = config.yaml_file['twitter']['sub_twitter'][account]['OAuthHandler']['consumer_key']
+        consumer_sec = config.yaml_file['twitter']['sub_twitter'][account]['OAuthHandler']['consumer_secret']
+        access_key = config.yaml_file['twitter']['sub_twitter'][account]['access_token']['key']
+        access_sec = config.yaml_file['twitter']['sub_twitter'][account]['access_token']['secret']
+        self.authorize = tweepy.OAuthHandler(consumer_key, consumer_sec)
+        self.authorize.set_access_token(access_key, access_sec)
+        self.api = tweepy.API(self.authorize, wait_on_rate_limit=True)
+        self.supported_types = ['jpg', 'png', 'gif', 'webp', 'mp3', 'mp4']
+
+    def alert(self, message, filename=None, verbose=False):
+        try:
+            if verbose:
+                print(f"==== [ Tweepy input ] ======\n"
+                      f"{message}\n")
+
+            # only attach media if the filepath is provided and is a supported file type.
+            # .jpg, .png, .gif, .mp3, .mp4
+            if filename and filename.split('.')[1] in self.supported_types:
+                media = self.api.media_upload(filename)
+                self.api.update_status(status=message, media_ids=[media.media_id])
+                print("🐤 tweet successfully sent with media!")
+                time.sleep(5)
+            else:
+                self.api.update_status(status=message)
+                print("🐤 tweet successfully sent!")
+                time.sleep(5)
+
+        except Exception as tweepy_err:
+            if tweepy_err == "[{'code': 187, 'message': 'Status is a duplicate.'}]":
+                pass
+            else:
+                print(f"An error has occurred with Tweepy: {tweepy_err}")
