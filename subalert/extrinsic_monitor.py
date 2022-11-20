3bf8a773d439f08e9ca08051f89b59b9bef8288a
nadro-j <n4dro@protonmail.com>
2022-11-20 00:21:55 +0000

    interrogate each block in a thread

--- subalert/extrinsic_monitor.py
+++ subalert/extrinsic_monitor.py
@@ -1,4 +1,6 @@
 import asyncio
+import threading
+from threading import Thread
 
 from subalert.base import Utils
 from subalert.logger import log_events
@@ -15,14 +17,16 @@ log = log_events(filename='extrinsic-monitor.log', debug=False)
 
 class ExtrinsicMonitor:
     def __init__(self):
-        self.previous_hash = []
+        self.previous_block_hash = []
+        self.sema = threading.Semaphore(value=10)
         self.config = Configuration()
         self.substrate = self.config.substrate
 
         # Queue events
         self.loop = asyncio.get_event_loop()
 
-    def extrinsic(self, block, extrinsic_types, check_receipt):
+    def inspect_extrinsic(self, block, extrinsic_types, check_receipt):
+        self.sema.acquire()
         result = self.substrate.get_block(block_number=block, ignore_decoding_errors=True)
         block_hash, extrinsics_list = result['header']['hash'], []
 
@@ -45,8 +49,39 @@ class ExtrinsicMonitor:
                         continue
 
                     extrinsics_list.append(extrinsic.value)
-        log.info(f"{len(extrinsics_list)} extrinsic(s) were successfully signed")
+        log.info(f"{len(extrinsics_list)} extrinsic(s) were successfully signed on block {block}")
+        self.process_extrinsics(extrinsics=extrinsics_list)
+        self.sema.release()
         return block_hash, extrinsics_list
+
+    def process_extrinsics(self, extrinsics):
+        block_event_construct = {"transactions": [], 'batch_all': []}
+
+        # Pass all successful extrinsics to the appropriate function for parsing.
+        # file: extrinsic_parser.py
+        for extrinsic in extrinsics:
+            if extrinsic is not None:
+                call_type = extrinsic['call']['call_function']
+                extrinsic_data = ParseExtrinsic(extrinsic)
+
+                if call_type == 'transfer':
+                    transaction = extrinsic_data.transactions
+                    block_event_construct['transactions'].append(transaction)
+
+                elif call_type == 'batch_all':
+                    batch_all = extrinsic_data.remark_batch_all
+                    if batch_all:
+                        block_event_construct['batch_all'].append(batch_all)
+
+        queue.enqueue(block_event_construct)
+
+        # Run queue task if size is >= 1, clear queue & block_event_construct once complete.
+        if queue.size() >= 1:
+            task = self.loop.create_task(queue.process_queue())
+            self.loop.run_until_complete(task)
+
+        queue.clear()
+        block_event_construct.clear()
 
     def new_block(self, obj, update_nr, subscription_id):
         """
@@ -56,46 +91,22 @@ class ExtrinsicMonitor:
         :return: When a new block occurs, it is checked against check_transaction to see if the amount transacted is
                  greater than the threshold set.
         """
-        block = obj['header']['number'] - 50  # 50 blocks behind
-        bhash = obj['header']['parentHash']
-
-        block_event_construct = {"transactions": [], 'batch_all': []}
+        block = obj['header']['number'] - 20  # 25 blocks behind
+        parent_block_hash = obj['header']['parentHash']
         calls = ['batch_all', 'remark', 'transfer']
 
         # check current hash and previous hash to mitigate
         # duplicate blocks whilst subscribed
-        if bhash not in self.previous_hash:
-            if len(self.previous_hash) >= 1:
-                self.previous_hash.pop(len(self.previous_hash) - 1)
-            self.previous_hash.append(bhash)
+        if parent_block_hash not in self.previous_block_hash:
+            if len(self.previous_block_hash) >= 1:
+                self.previous_block_hash.pop(len(self.previous_block_hash) - 1)
+            self.previous_block_hash.append(parent_block_hash)
 
+            log.info("")
             log.info(f"🔨 New block: {block} produced by: {obj['author']}")
-            blockhash, extrinsics = self.extrinsic(block=block, extrinsic_types=calls, check_receipt=True)
-            log.info(f"Monitored extrinsics found: {len(extrinsics)}")
+            thread = Thread(target=self.inspect_extrinsic, daemon=True, args=(block, calls, True,))
 
-            for extrinsic in extrinsics:
-                if extrinsic is not None:
-                    call_type = extrinsic['call']['call_function']
-                    extrinsic_data = ParseExtrinsic(extrinsic)
-
-                    if call_type == 'transfer':
-                        transaction = extrinsic_data.transactions
-                        block_event_construct['transactions'].append(transaction)
-
-                    elif call_type == 'batch_all':
-                        batch_all = extrinsic_data.remark_batch_all
-                        if batch_all:
-                            block_event_construct['batch_all'].append(batch_all)
-
-            queue.enqueue(block_event_construct)
-
-            # Run queue task if size is >= 1, clear queue & block_event_construct once complete.
-            if queue.size() >= 1:
-                task = self.loop.create_task(queue.process_queue())
-                self.loop.run_until_complete(task)
-
-            queue.clear()
-            block_event_construct.clear()
-
+            thread.start()
+            thread.join()
         else:
             pass
